fa23e07bd1025da4cc217b478a9731db66e5f8ca
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-07-18 12:16:25 +0530

    Integrate Prisma on NestJS

--- apps/nest/src/app.service.ts
+++ apps/nest/src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AppService {
+  constructor(public readonly prisma: PrismaService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getUsers(): Promise<User[]> {
+    const users = await this.prisma.user.findMany();
+    return users;
   }
 }
